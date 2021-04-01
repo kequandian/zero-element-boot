38d074a5fbe46a3a060dd70dab735ef2f31255bf
HDJ <263463764@qq.com>
2021-04-01 16:47:16 +0800

    新增 NamedCartConfig, NamedPresenterConfig，用于保存自定义组件

--- src/components/index.js
+++ src/components/index.js
@@ -11,6 +11,23 @@ import AutoComponent from './AutoComponent';
 
 import APIContainer from './container/APIContainer';
 
+//set components config
+import { set as NamedCartSet } from '@/config/NamedCartConfig';
+import { set as NamedPresenterSet } from '@/config/NamedPresenterConfig';
+
+//cart
+import ItemCart from './cart/ItemCart';
+import HightlightCart from './cart/HightlightCart';
+import HoverShadowCart from './cart/HoverShadowCart';
+import SelectCart from './cart/SelectCart';
+
+NamedCartSet({
+  ItemCart,
+  HightlightCart,
+  HoverShadowCart,
+  SelectCart
+})
+
 // default to export core components
 export {
         AutoLayout,
