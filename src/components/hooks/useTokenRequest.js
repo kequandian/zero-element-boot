a4b3d9b1bd2f4e3327e4a83b74d9e69d32f0a2f0
HDJ <263463764@qq.com>
2021-05-19 14:10:26 +0800

    useTokenRequest 新增 id 属性

--- src/components/hooks/useTokenRequest.js
+++ src/components/hooks/useTokenRequest.js
@@ -1,12 +1,16 @@
 const { useEffect, useState } = require('react');
 const promiseAjax = require('@/components/utils/request');
 
-module.exports = function useTokenRequest({api, bindFiles, requestData = {}, accountToken = ''}) {
-   
+module.exports = function useTokenRequest({ api, bindFiles, requestData = {}, accountToken = '' }, callBack) {
+
     const [data, setRespData] = useState([]);
 
+    const [useId, setId] = useState('');
+
     const [postData, setPostData] = useState('');
-    
+
+    const [mApi, setMApi] = useState(api);
+
     useEffect(() => {
 
         const reqData = requestData;
@@ -21,43 +25,61 @@ module.exports = function useTokenRequest({api, bindFiles, requestData = {}, acc
         }
 
         if (api) {
-            promiseAjax(api, reqData, options)
-                .then(responseData => {
-                    if (responseData && responseData.code === 200) {
-                        let data = responseData.data;
-                        
-                        if(bindFiles){
-                            if(Array.isArray(data)){
-                                const newList = [];
-                                data.map(item => {
-                                    newList.push(doBind(bindFiles, item));
-                                })
-                                data = newList;
-                            }else{
-                                data = doBind(bindFiles, responseData.data);
-                            }
-                        }
-                        setRespData(data)
-                    }
-                })
-        }else{
+
+            if (useId) {
+                reqData.token = accountToken;
+            }
+
+            query(mApi, reqData, options)
+
+        } else {
             console.warn('API为空, 访问被拒绝');
         }
 
 
-    }, [postData]);
+    }, [useId, postData]);
+
+    function query(api, reqData, options) {
+
+        promiseAjax(api, reqData, options)
+            .then(responseData => {
+                if (responseData && responseData.code === 200) {
+                    let data = responseData.data;
+
+                    if (bindFiles) {
+                        if (Array.isArray(data)) {
+                            const newList = [];
+                            data.map(item => {
+                                newList.push(doBind(bindFiles, item));
+                            })
+                            data = newList;
+                        } else {
+                            data = doBind(bindFiles, responseData.data);
+                        }
+                    }
+                    setRespData(data);
+                    callBack(data)
+                }
+            })
+    }
 
     function setData(data) {
         setPostData(data)
     }
+    
+    //根据ID获取数据
+    function changeData({ id, api }) {
+        setId(id);
+        setMApi(api);
+    }
 
-    function doBind(binding, data={}) {
+    function doBind(binding, data = {}) {
         let bindingData = {}
         Object.keys(binding).forEach(key => {
-          bindingData[binding[key]] = data[key];
+            bindingData[binding[key]] = data[key];
         })
         return { ...bindingData };
-      }
+    }
 
-    return [data, setData];
+    return [data, setData, changeData];
 }
