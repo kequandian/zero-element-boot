5d15f377ff71dac5792f8ce0cbe1f89dbd42dedd
HDJ <263463764@qq.com>
2021-05-28 17:53:01 +0800

    移除 WrappedGrid

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,6 +1,4 @@
-import Align from './Align';
 import Grid from './Grid/Index';
-import WrappedGrid from './WrappedGrid';
 import Flexbox from './Flexbox';
 import Itembox from './Itembox';
 import SelectBox from './SelectBox';
@@ -8,15 +6,15 @@ import SelectBox from './SelectBox';
 
 import Wrap from './Wrap';
 import Stack from './Stack';
+import Round from "./Round";
 
 export {
   Flexbox,
   Itembox,
   Grid,
-  Align,
-  WrappedGrid,
   SelectBox,
 
   Wrap,
-  Stack
+  Stack,
+  Round
 }
