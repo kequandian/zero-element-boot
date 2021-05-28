e102b128262134678da42e8584caafd1b97e0918
HDJ <263463764@qq.com>
2021-05-28 18:04:09 +0800

    移除 SelectBox

--- mock/ComponentListData.js
+++ mock/ComponentListData.js
@@ -16,11 +16,6 @@ export default {
             "pathUrl": "",
             "imageUrl": "https://img.36krcdn.com/20201225/v2_1fb3736a857d4c918a6ea0431ebce55b_img_jpg",
           },
-          {
-            "name": "SelectBox",
-            "pathUrl": "",
-            "imageUrl": "https://img.36krcdn.com/20201224/v2_0859a3ca4ddb49b4b8ff878270f07942_img_png",
-          },
         ]
       },
       {
