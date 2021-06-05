090bd13bf9828d05168493753df0009f9643d0ad
huawei laptop <huawei@laptop.com>
2021-06-05 12:06:05 +0800

    TestNamedPresenterConfig.js

--- src/global.js
+++ src/global.js
@@ -9,12 +9,18 @@ import ImageAnimation from '@/pages/PresenterTestDemo/components/presenter/item/
 import ContentText from '@/pages/PresenterTestDemo/plugins/TodoList/TodoItem/Content_text'
 import ContentFinish from '@/pages/PresenterTestDemo/components/presenter/item/ItemIconAction'
 
+import {Avator, Title} from '@/presenter/demo'
+
 // NamedCartSet({
 //   ItemCart
 // })
 
 NamedPresenterSet({
+  Avator,
+  Title,
+
   ImageAnimation,
   ContentText,
   ContentFinish
 })
+
