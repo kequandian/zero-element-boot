457f96720cdbc182f15ecab030a05e396c381ab5
vincent@macbook <vincent@macbook.air>
2021-06-06 09:51:22 +0800

    Clean up demo

--- src/composition/index.js
+++ src/composition/index.js
@@ -1,20 +1,8 @@
 
-import CartItem from './CartItem';
 import AvatorItem from './AvatarItem';
-import ImageItem from './ImageItem';
-import SelectItem from './SelectItem';
-import RoundItem from './RoundItem';
-import RectangleItem from './RectangleItem';
-import CircleItem from './CircleItem';
 import AdItem from './AdItem';
 
 export {
-    CartItem,
     AvatorItem,
-    ImageItem,
-    SelectItem,
-    RoundItem,
-    RectangleItem,
-    CircleItem,
     AdItem,
 }
