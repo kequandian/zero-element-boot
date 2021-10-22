37d4508b246896c41f1cb89dbb81f59ce15db4eb
HDJ <263463764@qq.com>
2021-10-22 18:47:44 +0800

    新增 boot-x文件夹

---
+++ boot-x/zero-element-boot-presenter/src/index.js
@@ -0,0 +1,23 @@
+import Default from './components/presenter/Default';
+import Avatar from './components/presenter/Avatar';
+import Title from './components/presenter/Title';
+import Subtitle from './components/presenter/Subtitle';
+import ImageAnimation from './components/presenter/ImageAnimation';
+import Description from './components/presenter/Description';
+import Author from './components/presenter/Author';
+import Seperator from './components/presenter/Seperator';
+import StatisticsList from './components/presenter/StatisticsList';
+import TableCompx from './components/presenter/TableCompx';
+
+export {
+  Default,
+  Avatar,
+  Title,
+  Subtitle,
+  ImageAnimation,
+  Description,
+  Author,
+  Seperator,
+  StatisticsList,
+  TableCompx
+}
---
+++ boot-x/zero-element-boot-selectedCart/src/index.js
@@ -0,0 +1,10 @@
+
+import SelectIndicatorCart from './components/selectedCart/SelectIndicatorCart';
+import SelectedCartRightIcon from './components/selectedCart/SelectedCartRightIcon';
+import SelectedCartUpperRightIcon from './components/selectedCart/SelectedCartUpperRightIcon';
+
+export {
+    SelectIndicatorCart,
+    SelectedCartRightIcon,
+    SelectedCartUpperRightIcon
+}
