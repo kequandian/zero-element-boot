a482421808013fc242dc8ebf731d1d0868ee76eb
HDJ <263463764@qq.com>
2021-11-04 18:24:08 +0800

    测试ProgressList组件

---
+++ src/composition/StatisticsContainer/ProgressList/layout.js
@@ -0,0 +1,17 @@
+module.exports = {
+  xname: 'Flexbox',
+  props: {
+    align: 'start',
+    direction: 'column'
+  },
+  cart: {
+    xname: 'Cart',
+    props: {
+      isOnHover:false,
+      margin: '2px 0px 2px 0px',
+      linewidth: 0,
+      padding: '0px'
+    }
+  },
+  container: 'PlainList'
+}
