2858acff7a9b8f8b37f56f6ec238e74d7e7eec30
HDJ <263463764@qq.com>
2021-11-18 13:42:17 +0800

    新增 l2, l3

--- src/composition/StatisticsContainer/StatisticsBody/_layout.js
+++ src/composition/StatisticsContainer/StatisticsBody/_layout.js
@@ -79,7 +79,35 @@ module.exports = {
                       }
                     }
                   },
-                  presenter: 'PregressBody',
+                  presenter: {
+                    layout: {
+                      xname: 'Flexbox',
+                      props: {
+                        align: 'start',
+                        direction: 'column',
+                        flexWidth: 'auto-full'
+                      },
+                      gateway: {
+                        xname: 'Binding',
+                        props: {
+                          binding: {
+                            items: 'items'
+                          }
+                        }
+                      },
+                      presenter: 'PregressBody',
+                      cart: {
+                        xname: 'Cart',
+                        props: {
+                          isOnHover:false,
+                          margin: '2px 0px 2px 0px',
+                          linewidth: 0,
+                          padding: '0px'
+                        }
+                      },
+                      container: 'PlainList',
+                    }
+                  },
                   cart: {
                     xname: 'Cart',
                     props: {
