8f9f8b23322a6340246ee04a4300906a1f50c141
HDJ <263463764@qq.com>
2021-12-18 17:02:25 +0800

    对接standalone api

--- src/composition/Standalone/index.js
+++ src/composition/Standalone/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Flex, Center, Box, Stack, Spacer  } from "@chakra-ui/react";
+import { Flex, Center, Box, Stack, Spacer, VStack, Container  } from "@chakra-ui/react";
 import { AutoLayout } from '@/components';
 const promiseAjax = require('@/components/utils/request');
 
@@ -17,6 +17,12 @@ export default function Index(props) {
     const layoutJsonPath = '';
     const localLayoutJson = layout;
 
+    let api = '/dev/dependency/decompile/json';
+
+    if (process.env.NODE_ENV === 'development') {
+      api = `http://192.168.3.121:8080${api}`;
+    }
+
     if(layoutJsonPath){
         layoutData = { path: layoutJsonPath};
     }else{
@@ -29,25 +35,52 @@ export default function Index(props) {
     };
 
     const onJarItemClick = (item) => {
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
         let name = item.value;
-        if(name.indexOf('/') > -1){
-            const list = name.split('/');
-            name = list[list.length-1]
+        // if(name.indexOf('/') > -1){
+        //     const list = name.split('/');
+        //     name = list[list.length-1]
+        // }
+        if(name.indexOf('@') > -1){
+            const list = name.split('@');
+            name = list[0]
         }
+        setDetail([])
         getDetailFetch(name)
     }
 
-
+    //
     const getDetailFetch = async (name) => {
-        const api = `http://localhost:8080/api/dev/dependency/decompile`;
+        // const api = `http://localhost:8080/api/dev/dependency/decompile`;
         promiseAjax(api, {pattern:name}, {})
             .then(responseData => {
                 if (responseData && responseData.code === 200) {
                     let respData = responseData.data;
-
                     console.log('respData = ', respData)
+                    setDetail(respData);
                 }
             })
+    }
+
+    //处理返回内容
+    function handleContent(data){
+
+        if(typeof data === 'string'){
+            return data;
+        }if(data instanceof Array && data.length > 0){
+            return (
+                <Stack>
+                    {
+                        data.map((item, index) => {
+                            return  <Container maxW='container.xl'>{item}</Container>
+                        })
+                    }
+                </Stack>
+            )
+        }else{
+            return '';
+        }
+
     }
 
 
@@ -59,9 +92,11 @@ export default function Index(props) {
                 </AutoLayout>
             </Box>
             <Box flex='1'>
-                <div style={{background:'#ffffff', width:'100%', padding: '10px', marginTop:'8px'}}>
-                    {showDetail}
-                </div>
+                { showDetail && showDetail.length > 0 ? (
+                    <div style={{background:'#ffffff', width:'100%', padding: '10px', marginTop:'8px'}}>
+                        {handleContent(showDetail)}
+                    </div>
+                ): null}
             </Box>
         </Flex>
     )
