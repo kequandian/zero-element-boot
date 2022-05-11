ee48ecf121bfd501398a0a4e4e7a7fb7b53810d1
HDJ <263463764@qq.com>
2022-05-11 14:54:01 +0800

    DownloadCompx => DownloadButton

--- src/pages/TestDownloadDemo/index.js
+++ src/pages/TestDownloadDemo/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { ChakraProvider } from "@chakra-ui/react";
 
-import { DownloadCompx } from '@/presenter/demo';
+import { DownloadButton } from '@/presenter/demo';
 
 
 export default function Index(props) {
-
+    
+    /**
+     * @param { 下载API, 可拼接endpoint } downloadApi
+     * @param { 问号后面的参数 } query
+     */
     const requestData = {
         downloadApi: '/dev/connection/snapshot/instant',
         query: {
@@ -15,7 +19,7 @@ export default function Index(props) {
 
     return (
         <ChakraProvider>
-            <DownloadCompx {...requestData}/>
+            <DownloadButton {...requestData}/>
         </ChakraProvider>
     )
 }
