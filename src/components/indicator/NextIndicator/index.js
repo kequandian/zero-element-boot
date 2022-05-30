f1c97a1fd46c78eb0f92209e39523bd2f2e37acb
m.2@sandbox <m.2@sandbox>
2022-05-30 18:40:23 +0800

    update NextIndicator

--- src/components/indicator/NextIndicator/index.js
+++ src/components/indicator/NextIndicator/index.js
@@ -1,13 +1,27 @@
 const React = require('react');
 
-//将接收到的所有参数传递给子组件
 
+/**
+ * 用于将接收到的所有参数传递给子组件
+ * 相当于一个无效果的仅传递参数的子组件
+ * 
+ * */
 export default function NextIndicator({ children, ...rest }) {
-    return (<div >
-        {React.Children.toArray(children).map(child => {
-        return React.cloneElement(child, {
-            ...rest
-        })
-        })}
-    </div>)
+    return (
+        <>
+            {React.Children.toArray(children).map(child => {
+                return React.cloneElement(child, {
+                    ...rest
+                })
+            })}
+        </>)
+    // return React.Children.map(children, child => {
+    //     return (
+    //     <>
+    //         return React.cloneElement(child, {
+    //             ...rest
+    //             })
+    //     </>
+    //     )
+    //   })
 }
