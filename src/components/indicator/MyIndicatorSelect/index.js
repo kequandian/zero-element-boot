c74e272d1ba07ad8aa597c6dd91540649b5f22b8
HDJ <263463764@qq.com>
2022-05-30 15:17:30 +0800

    优化部分indicator

--- src/components/indicator/MyIndicatorSelect/index.js
+++ src/components/indicator/MyIndicatorSelect/index.js
@@ -5,11 +5,12 @@ import React, { useImperativeHandle, forwardRef } from 'react';
 
 export default  forwardRef(function MyIndicatorSelect(props, ref) {
 
-  const { children, indicate } = props;
+  const { children, indicate, defaultHoverStyles } = props;
 
   const styles = {
     borderColor: 'transparent',
-    boxShadow: '0 0px 6px rgba(255, 255, 255, 1)'
+    boxShadow: '0 0px 6px rgba(255, 255, 255, 1)',
+    ...defaultHoverStyles
   }
 
   useImperativeHandle(ref, () => ({
