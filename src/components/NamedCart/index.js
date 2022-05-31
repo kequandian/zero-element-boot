e8802556f43eb845987eed611290e1ce6bb08fce
m.2@sandbox <m.2@sandbox>
2022-05-31 16:13:28 +0800

    update NamedSelector, and remove selector/Selector

--- src/components/NamedCart/index.js
+++ src/components/NamedCart/index.js
@@ -1,14 +1,17 @@
-const React = require('react');
-const { forwardRef } = require('react');
-const useLayout = require('@/components/hooks/useLayout');
+// const React = require('react');
+import React from 'react';
+// const { forwardRef } = require('react');
+// import { forwardRef } from 'react';
+// const useLayout = require('@/components/hooks/useLayout');
+// import useLayout from 'lib/components/hooks/useLayout';
+
 // const DefaultCartSet = require('../cart');
 import { get as DefaultCartSet } from '@/components/config/NamedCartConfig';
 
 // indicator 属于 CART, 这里考虑分开管理
 import { get as DefaultIndicatorSet } from '@/components/config/NamedIndicatorConfig';
 
-//
-import NamedSelector from '@/components/NamedSelector';
+import NamedSelector from 'lib/components/NamedSelector';
 
 
 /**
@@ -16,33 +19,36 @@ import NamedSelector from '@/components/NamedSelector';
  * 区别于 NamedGateway 数据传递由具体的 Gateway 处理
  * @param {} xname 引用的Cart的名称
  * @param {} props 引用的Cart的属性
+ * @param {} cart 参数格式 car={xname:'Cart', props: {}}
  * @param {} indicator 鼠标hover时的Cart的名称
  * @param {} selector 选中的时候的Cart的名称
- * @param {} cart 参数格式 car={xname:'Cart', props: {}}
+ * @param {} defaultIndicator 默认状态下的叠加的Cart
  */
-export default function NamedCart({ children, xname, indicator, selector, defaultIndicator, props, cart = { xname, props }, cartSet, indicatorSet, ...rest }) {
+export default function NamedCart({ children, xname, props, cart = { xname, props }, cartSet,  /*multi indicator*/ indicator, selector, defaultIndicator, indicatorSet, isSelected, /* end indicator*/    ...rest }) {
 
-  
   const _CartSet = cartSet ? cartSet : DefaultCartSet()
   const cartName = (typeof cart === 'string') ? cart : cart.xname
   const _Cart = _CartSet[cartName] || tips(cartName);
 
-  //2021-03-27 新增 indicator 模块
-  //const  _Indicator  = _CartSet[indicator];
-  
   //2021-10-28 新增 selector 模块
   const _IndicatorSet = indicatorSet ? indicatorSet : DefaultIndicatorSet()
+  
+
   // get indicator
-  const indicatorName = (typeof indicator === 'string') ? indicator : (typeof indicator === 'object') ? indicator.xname : '';
-  const    _Indicator  = _IndicatorSet[indicatorName];
+  const indicatorName = (typeof indicator === 'string') ? indicator : (typeof indicator === 'object') ? indicator.xname : ''
+  const    _Indicator  = _IndicatorSet[indicatorName] || tips(indicatorName)
+  const indicatorProps = (typeof indicator === 'object') ? indicator.props : {}
+
   // get selector
-  const selectorName = (typeof selector === 'string') ? selector : (typeof selector === 'object') ? selector.xname : '';
-  const    _Selector  = _IndicatorSet[selectorName];
+  const selectorName = (typeof selector === 'string') ? selector : (typeof selector === 'object') ? selector.xname : ''
+  const    _Selector  = _IndicatorSet[selectorName] || tips(selectorName)
+  const selectorProps = (typeof selector === 'object') ? selector.props : {}
 
   //2021-10-28 新增 defaultIndicator 模块
-  // get defaultIndicator
-  const defaultIndicatorName = (typeof defaultIndicator === 'string') ? defaultIndicator : (typeof defaultIndicator === 'object') ? defaultIndicator.xname : '';
-  const    _DefaultIndicator  = _IndicatorSet[defaultIndicatorName];
+  // get defaultIndicator, the same as _Cart
+  const defaultIndicatorName = (typeof defaultIndicator === 'string') ? defaultIndicator : (typeof defaultIndicator === 'object') ? defaultIndicator.xname : ''
+  const _DefaultIndicator  = _IndicatorSet[defaultIndicatorName] || tips(defaultIndicatorName)
+  const defaultIndicatorProps = (typeof defaultIndicator === 'object') ? defaultIndicator.props : {}
 
   return (
     <>
@@ -58,12 +64,15 @@ export default function NamedCart({ children, xname, indicator, selector, defaul
             //     })}
             //   </_Cart>
             // </_Indicator>
-            <NamedSelector hoverIndicator={_Indicator} selectedIndicator={_Selector} defaultIndicator={_DefaultIndicator}  {...rest} >
-              <_CartModule children={children} Cart={_Cart} props={cart.props} data={rest} />
+
+            <NamedSelector defaultIndicator={_DefaultIndicator} defaultIndicatorProps={...defaultIndicatorProps} 
+                           hoverIndicator={_Indicator}  hoverIndicatorProps = {...indicatorProps}
+                           selectedIndicator={_Selector}  selectedIndicatorProps = {...selectorProps}   {...rest} >
+                <_CartModule children={children} Cart={_Cart} props={cart.props} data={rest} /> 
             </NamedSelector>
         ) : 
         (
-           <_CartModule children={children} Cart={_Cart} props={cart.props} data={rest} />
+            <_CartModule children={children} Cart={_Cart} props={cart.props} data={rest} /> 
         )
       }
     </>
@@ -79,6 +88,7 @@ function _CartModule({children, Cart, props, data}){
             })}
         </Cart>)
 }
+
 
 function tips(name) {
   return _ => `NamedCart ${name} 未定义`;
--- src/components/NamedSelector/index.js
+++ src/components/NamedSelector/index.js
@@ -1,64 +1,54 @@
 import React, { useState } from 'react';
 import useLayout from '@/components/hooks/useLayout';
-import { get as DefaultIndicatorSet } from '@/components/config/NamedIndicatorConfig';
-import NextIndicator from '@/components/indicator/NextIndicator';
 
+// 无效果，仅传递数据
+import NextIndicator from '@/components/indicator/NextIndicator';
 
 /**
  * 
- * @param {Object} defaultIndicator 默认样式组件 ( type of Cart)
- * @param {String} decorator        命名的默认样式的名称 ( type of Cart)
- * @param {Object} hoverIndicator   响应Hover事件的Indicator ( type of Indicator)
- * @param {String} indicator        命名的响应Hover事件的Indicator的名称 ( type of Indicator)
- * @param {Object} selectedIndicator 响应Selected事件的Indicator (type of Indicator)
- * @param {Object} selector         命名的 响应Selected事件的Indicator的名称 (type of Indicator)
- * @param {Boolean} isSelected      是否选中状态，由父组件决定
+ * @param {Component} defaultIndicator   默认的Cart
+ * @param {Object} defaultIndicatorProps defaultIndicator 参数
+ * @param {Component} hoverIndicator     鼠标Hover状态下的Cart, 叠加效果
+ * @param {Object} hoverIndicatorProps   hoverIndicator 参数
+ * @param {Component} selectedIndicator  选中状态下的Cart, 替换默认的 Cart
+ * @param {Object} selectedIndicatorProps   selectedIndicator 参数
  * @returns 
  */
-export default function NamedSelector(props) {
+ function NamedSelector( { children, defaultIndicator={}, defaultIndicatorProps = {},
+                                    hoverIndicator = {}, hoverIndicatorProps = {},
+                                    selectedIndicator={}, selectedIndicatorProps={}, isSelected=false}) {
 
-  const { children, defaultIndicator={}, decorator={}, hoverIndicator = {}, indicator={}, selectedIndicator={}, selector={},  isSelected=false} = props;
-  
-  const [hoverRef, { getHoverStyles }] = useLayout();
-  const [selectRef, { getSelectStyles }] = useLayout();
+const [hoverRef, { getHoverStyles }] = useLayout();
+const [selectRef, { getSelectStyles }] = useLayout();
 
-  
-  const [onHover, setOnHover] = useState(false);
-  
-  const toggleHover = () => {
-    const result = !onHover;
-    setOnHover(result)
-  }
+const [onHover, setOnHover] = useState(false);
 
-  // indicator set
-  const _IndicatorSet =  DefaultIndicatorSet()
+const toggleHover = () => {
+const result = !onHover;
+setOnHover(result)
+}
 
-  // default, hover, selected style
-  const hoverIndicatorName = indicator===undefined ? undefined : ( (typeof indicator === 'string') ? indicator : (typeof indicator === 'object') ? indicator.xname : undefined );
-  const HoverIndicator =  ( (hoverIndicator===undefined && indicator===undefined) || onHover==false ) ? NextIndicator : 
-                          (hoverIndicator===undefined ? (_IndicatorSet[hoverIndicatorName]) : hoverIndicator )
+// 选中状态无需 default indicator, means cart
+const DefaultIndicator =  (defaultIndicator===undefined || isSelected) ? NextIndicator : defaultIndicator
 
-  const selectedIndicatorName = selector===undefined ? undefined : ( (typeof selector === 'string') ? selector : (typeof selector === 'object') ? selector.xname : undefined );
-  const SelectedIndicator = ( (selectedIndicator===undefined && selector===undefined) || isSelected == false )? NextIndicator : 
-                          (selectedIndicator===undefined ? (_IndicatorSet[selectedIndicatorName]) : selectedIndicator )
+// 没有传入 selectedIndicator, 或 isSelected==false, 相当于没有效果
+const SelectedIndicator = (selectedIndicator===undefined || isSelected == false )? NextIndicator : selectedIndicator
 
-  const defaultIndicatorName = decorator===undefined ? undefined : ( (typeof decorator === 'string') ? decorator : (typeof decorator === 'object') ? decorator.xname : undefined );
-  const DefaultIndicator =  (defaultIndicator===undefined && decorator===undefined) ? NextIndicator : 
-                          (defaultIndicator===undefined ? (_IndicatorSet[defaultIndicatorName]) : defaultIndicator )
+// default, hover, selected style
+const HoverIndicator =  ( hoverIndicator===undefined || onHover==false ) ? NextIndicator : hoverIndicator
 
-  return React.Children.map(children, child => {
-    
-    return (
-      <div style={{flex: 1}} onMouseEnter={() => toggleHover()} onMouseLeave={() => toggleHover()}>
-          <DefaultIndicator getHoverStyles={getHoverStyles} getSelectStyles={getSelectStyles}>
-            <HoverIndicator ref={hoverRef}>
-              <SelectedIndicator ref={selectRef}>
-              {child}
-              </SelectedIndicator>
-            </HoverIndicator>
-          </DefaultIndicator>
-      </div>
-      
+return React.Children.map(children, child => {
+  return (
+    <div style={{flex: 1}} onMouseEnter={() => toggleHover()} onMouseLeave={() => toggleHover()}>
+      <DefaultIndicator getHoverStyles={getHoverStyles} getSelectStyles={getSelectStyles} {...defaultIndicatorProps}>
+          {/* DefaultIndicator/SelectedIndicator 二选一 */}
+          <HoverIndicator ref={hoverRef} {...hoverIndicatorProps}>
+          <SelectedIndicator ref={selectRef} {...selectedIndicatorProps}>
+             {child}
+          </SelectedIndicator>
+          </HoverIndicator>
+      </DefaultIndicator>
+    </div>
     )
   })
 }
