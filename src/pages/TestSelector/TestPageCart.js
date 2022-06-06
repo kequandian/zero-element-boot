7abdffc7e6f5a2a07aa5c23069b7f678eac5af9a
vincent@macbook <vincent@macbook.air>
2022-06-06 22:41:13 +0800

    from NamedSelector => OverlaySelector

--- src/pages/TestSelector/TestPageCart.js
+++ src/pages/TestSelector/TestPageCart.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Butter, Clean, Pink} from '@/components/presenter'
-import NamedSelector from '@/components/NamedSelector'
+import OverlaySelector from '@/components/OverlaySelector'
 import DefaultHoverIndicator from '@/components/indicator/DefaultHoverIndicator'
 
 export default function TestSelector(props){
     return(
-        <NamedSelector hoverIndicator={DefaultHoverIndicator}>
+        <OverlaySelector hoverIndicator={DefaultHoverIndicator}>
             <Butter />
-        </NamedSelector>
+        </OverlaySelector>
     )
 }
