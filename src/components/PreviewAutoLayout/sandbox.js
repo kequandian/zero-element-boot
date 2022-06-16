8c9a39984d8f709ab855f2f647275cca7350c9de
m.2@sandbox <m.2@sandbox>
2022-06-16 17:39:28 +0800

    update AutoLayout

--- src/components/PreviewAutoLayout/sandbox.js
+++ src/components/PreviewAutoLayout/sandbox.js
@@ -7,9 +7,11 @@ export default function (props) {
   // 获取要显示的数据的接口
   let api = '/api/crud/fieldModel/fieldModels'
   // 获取layoutJson的本地接口
-  let layoutJsonApi = ''
+  let layoutJsonApi = '/api/layoutJson'
+  // let layoutJsonApi = ''
   // 获取layoutJson的api接口，如果本地接口为空，则会使用该接口请求api
-  let layoutName = 'thisAutoLayout'
+  // let layoutName = 'thisAutoLayout'
+  let layoutName = ''
 
   return (
       <PreviewAutoLayout {...props} api={api} layoutApi={layoutJsonApi} layoutName={layoutName} />
