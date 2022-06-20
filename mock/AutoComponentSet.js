9148477a3c94c325323a9fe6164576784b1181a6
HDJ <263463764@qq.com>
2022-06-20 14:38:47 +0800

    修复OverlaySelector选中失效问题

---
+++ mock/AutoComponentSet.js
@@ -0,0 +1,45 @@
+export default {
+    'GET /api/AutoComponentSet': {
+                "code": 200,
+                     "data": [
+            
+
+        {
+            "_xname": "ItemPlaceholder",
+            "_props": {
+                "fill": "#52bbe8",
+                "bg":"#e5e5e5",
+              "size":160,
+
+             },
+        },
+
+        {
+            "_xname": "ItemPlaceholder",
+            "_props": {
+                "fill": "#52bbe8",
+                "bg":"#e5e5e5",
+              "size":160,
+
+             },
+        },
+
+        {
+            "_xname": "ItemPlaceholder",
+            "_props": {
+                "fill": "#52bbe8",
+                "bg":"#e5e5e5",
+              "size":160,
+
+             },
+        },
+
+
+      
+                ],
+
+                
+                "message": "Success"
+            }
+        }
+        
