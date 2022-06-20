7ac0b3da4c854dbbcf5e11674de96d66170362b3
cmj <2118188796@qq.com>
2022-06-20 09:55:47 +0800

    修改VStack

--- src/components/layout/VStack/index.js
+++ src/components/layout/VStack/index.js
@@ -2,20 +2,16 @@ import React from 'react';
 import Flexbox from '@/components/layout/Flexbox';
 import Container from '@/components/container/Container'
 
-export default function Round(props) {
-/**
- * 
- * @param {对齐方式: [start, center, end, around, between, start-with-last-end, align-content-center] } align
- * @param {子项对齐方式: start, center, end, [full, half, quad]: for item width } justify
- *
- */
-  const { children , justify='end', align = 'start', ...data } = props;
+export default function VStack(props) {
+
+
+    const { children, ...data } = props;
 
   return ( 
         <Container>
-            <Flexbox justify={justify}  align={align}  direction='column' {...data}>
+            <Flexbox direction='column' {...data}>
                 {children}
             </Flexbox>
-        </Container>
+        </Container> 
   )
 }
