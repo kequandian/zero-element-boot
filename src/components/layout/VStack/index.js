f64147b06bc4c2741e758c274d9c8a9fbc787164
cmj <2118188796@qq.com>
2022-06-20 18:39:25 +0800

    修改VStack参数说明

--- src/components/layout/VStack/index.js
+++ src/components/layout/VStack/index.js
@@ -2,20 +2,23 @@ import React from 'react';
 import Flexbox from '@/components/layout/Flexbox';
 import Container from '@/components/container/Container'
 
+
+/**
+ * 
+ * @param spacing 间隔
+ * 
+ */
+
 export default function VStack(props) {
-        
-    /**
-     * @param spacing 间隔
-     * 
-     */
+
 
     const { children, spacing, ...data } = props;
 
-    return ( 
-            <Container>
-                <Flexbox direction='column' spacing={spacing} {...data}>
-                    {children}
-                </Flexbox>
-            </Container> 
-  )
+    return (
+        <Container>
+            <Flexbox direction='column' spacing={spacing} {...data}>
+                {children}
+            </Flexbox>
+        </Container>
+    )
 }
