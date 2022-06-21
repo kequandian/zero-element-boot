1777a86c885ac95118b5037c1e31914b446025be
cmj <2118188796@qq.com>
2022-06-21 18:37:40 +0800

    修改Warp，新增AutoComponentSet

--- src/components/layout/Wrap/index.js
+++ src/components/layout/Wrap/index.js
@@ -8,8 +8,10 @@ export default function Wrap(props) {
   const { children, ...data } = props;
 
   return (
-        <Flexbox align='start' direction='row' {...data}>
-          {children}
-        </Flexbox>
+    <Container>
+      <Flexbox align='start' direction='row' {...data}>
+        {children}
+      </Flexbox>
+    </Container>
   )
 }
