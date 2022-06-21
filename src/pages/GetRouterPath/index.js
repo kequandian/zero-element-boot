6f936337c8c71a8f600987b33c08352d3bb064a9
m.2@sandbox <m.2@sandbox>
2022-06-21 14:33:34 +0800

    update useRouterParams

--- src/pages/GetRouterPath/index.js
+++ src/pages/GetRouterPath/index.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const useRouterParams = require('@/components/hooks/useRouterParams');
 
+// const useRouterParams = require('@/components/hooks/useRouterParams');
 
 export default function GetRouterPath(props) {
 
