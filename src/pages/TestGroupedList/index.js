93c31eae304d0a39dfadf9f21325973d9add97c2
vincent@macbook <vincent@macbook.air>
2022-06-29 21:49:26 +0800

    simple gateway as binding

--- src/pages/TestGroupedList/index.js
+++ src/pages/TestGroupedList/index.js
@@ -4,19 +4,19 @@ export default function(props){
 
   const groupdata = {
     "G1": [
-      {id: 1, name: "March", url: 'https://thirdwx.qlogo.cn/mmopen/vi_32/h5l7xksV3b6WE1H30xPpicqBdaibhrsrOHGCZhYdBGrnAb2kaVZsGYo2PLt9UcrqrdfDMcBhJ9IhBoV9MSMZmiaVw/132'},
-      {id: 2, name: "June", url: 'https://thirdwx.qlogo.cn/mmopen/vi_32/h5l7xksV3b6WE1H30xPpicqBdaibhrsrOHGCZhYdBGrnAb2kaVZsGYo2PLt9UcrqrdfDMcBhJ9IhBoV9MSMZmiaVw/132'}
+      {id: 1, name: "March", imageUrl: 'https://thirdwx.qlogo.cn/mmopen/vi_32/h5l7xksV3b6WE1H30xPpicqBdaibhrsrOHGCZhYdBGrnAb2kaVZsGYo2PLt9UcrqrdfDMcBhJ9IhBoV9MSMZmiaVw/132'},
+      {id: 2, name: "June", imageUrl: 'https://thirdwx.qlogo.cn/mmopen/vi_32/h5l7xksV3b6WE1H30xPpicqBdaibhrsrOHGCZhYdBGrnAb2kaVZsGYo2PLt9UcrqrdfDMcBhJ9IhBoV9MSMZmiaVw/132'}
     ], 
     "G2": [
       {id: 3, name: "Oct"},
-      {id: 2, name: "June", url: 'https://thirdwx.qlogo.cn/mmopen/vi_32/h5l7xksV3b6WE1H30xPpicqBdaibhrsrOHGCZhYdBGrnAb2kaVZsGYo2PLt9UcrqrdfDMcBhJ9IhBoV9MSMZmiaVw/132'},
+      {id: 2, name: "June", imageUrl: 'https://thirdwx.qlogo.cn/mmopen/vi_32/h5l7xksV3b6WE1H30xPpicqBdaibhrsrOHGCZhYdBGrnAb2kaVZsGYo2PLt9UcrqrdfDMcBhJ9IhBoV9MSMZmiaVw/132'},
       {id: 4, name: "Nov"}
     ]
   }
 
   const itemdata = [
-      {id: 1, name: "March", url: 'https://thirdwx.qlogo.cn/mmopen/vi_32/h5l7xksV3b6WE1H30xPpicqBdaibhrsrOHGCZhYdBGrnAb2kaVZsGYo2PLt9UcrqrdfDMcBhJ9IhBoV9MSMZmiaVw/132'},
-      {id: 2, name: "June", url: 'https://thirdwx.qlogo.cn/mmopen/vi_32/h5l7xksV3b6WE1H30xPpicqBdaibhrsrOHGCZhYdBGrnAb2kaVZsGYo2PLt9UcrqrdfDMcBhJ9IhBoV9MSMZmiaVw/132'},
+      {id: 1, name: "March", imageUrl: 'https://thirdwx.qlogo.cn/mmopen/vi_32/h5l7xksV3b6WE1H30xPpicqBdaibhrsrOHGCZhYdBGrnAb2kaVZsGYo2PLt9UcrqrdfDMcBhJ9IhBoV9MSMZmiaVw/132'},
+      {id: 2, name: "June", imageUrl: 'https://thirdwx.qlogo.cn/mmopen/vi_32/h5l7xksV3b6WE1H30xPpicqBdaibhrsrOHGCZhYdBGrnAb2kaVZsGYo2PLt9UcrqrdfDMcBhJ9IhBoV9MSMZmiaVw/132'},
       {id: 3, name: "Oct"},
       {id: 4, name: "Nov"}
     ]
@@ -24,9 +24,15 @@ export default function(props){
   // 
   const layout = {
         xname: 'Flexbox',
-        container: {xname: 'GroupedList'},
-        presenter: "ItemPlaceholder"
+        container: { xname: 'GroupedList' },
+        presenter: "ItemPlaceholder",
+        gateway: {
+          imageUrl: "url"
+        }
+    }
+
+  const gateway = {
   }
 
-  return <AutoLayout layout={layout} dataSource={groupdata} />
+  return <AutoLayout layout={layout} gateway={gateway} dataSource={groupdata} />
 }
