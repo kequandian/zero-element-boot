08d643823f35fd453f4d69d890fba15e8e5aba77
cmj <2118188796@qq.com>
2022-06-30 14:29:22 +0800

    提交Counter,修改useQuery

---
+++ src/components/presenter/button/DotDashButton/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+/**
+ * 
+ * @param {color} color 背景，边框，字体颜色
+ * @param {size} size 尺寸
+ * @param {borderColor} borderColor 边框颜色
+ * @param {fill} fill 填充颜色
+ * @param {onDashClick} onDashClick 点击事件
+ * 
+ */
+
+
+
+export default function Index(props) {
+
+    const { color = '#8e72ff', size = '30', borderColor, fill, onDashClick } = props;
+
+
+    const baseStyle = {
+        textAlign: 'center',
+        width: `${size}px`,
+        height: `${size}px`,
+        padding: '0'
+
+    }
+
+    return (
+        <div style={baseStyle} onClick={onDashClick} >
+            <svg width='100%' height='100%' >
+                <circle cx="50%" cy="50%" r={0.3 * size} stroke={borderColor} stroke-width="1.2" fill={fill} />
+                <line x1="50%" y1="35%" x2="50%" y2="65%" stroke={color} strokeWidth='1.2' />
+                <line x1="35%" y1="50%" x2="65%" y2="50%" stroke={color} strokeWidth='1.2' />
+
+            </svg>
+        </div>
+    )
+}
