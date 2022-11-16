e768820ef8457d4bb7a8a631c46a3d653982fa72
HDJ <263463764@qq.com>
2022-11-16 16:43:34 +0800

    优化打包脚本

--- gen.js
+++ gen.js
@@ -4,7 +4,7 @@ var fs = require('fs-extra');
 var distPath = path.resolve(process.cwd(), './dist');
 var publicPath = path.resolve(process.cwd(), './public');
 
-fs.moveSync(`${distPath}/dependency-ui/index.html`, `${distPath}/dependency-ui.html`);
+// fs.moveSync(`${distPath}/preview-ui/index.html`, `${distPath}/index.html`);
 fs.removeSync(`${distPath}/config.js`);
-fs.copyFileSync(`${publicPath}/config.js`, `${distPath}/config.js`);
+fs.copyFileSync(`${publicPath}/config.js`, `${distPath}/preview-ui/config.js`);
 // fs.moveSync(`${distPath}/static/x`, `${distPath}/x`);
