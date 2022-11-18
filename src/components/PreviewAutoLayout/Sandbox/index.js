57c423fa6bf8b2372f7e8cba9442cf1beb64d2d3
HDJ <263463764@qq.com>
2022-11-18 17:17:04 +0800

    getAutoLayOut => getAutoLayout

--- src/components/PreviewAutoLayout/Sandbox/index.js
+++ src/components/PreviewAutoLayout/Sandbox/index.js
@@ -41,12 +41,12 @@ export default function (props) {
   // 获取layoutJson的本地接口
   // let layoutJsonApi = '/api/layoutJson'
 
-  // let layoutJsonApi = `/openapi/lc/module/getAutoLayOut/autoLayOut`
+  // let layoutJsonApi = `/openapi/lc/module/getAutoLayout/autoLayOut`
 
   let layoutJsonApi = params.layoutJsonApi
 
   // if (process.env.NODE_ENV === 'development') {
-  //   layoutJsonApi = 'http://192.168.3.112:8080/openapi/lc/module/getAutoLayOut/autoLayOut'
+  //   layoutJsonApi = 'http://192.168.3.112:8080/openapi/lc/module/getAutoLayout/autoLayOut'
   // }
 
   // 获取layoutJson的api接口，如果本地接口为空，则会使用该接口请求api
