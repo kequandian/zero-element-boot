1f3b29867b744dced3b3c9eaa823ca13ad8dbef0
HDJ <263463764@qq.com>
2022-12-02 15:32:59 +0800

    优化 Text组件

--- src/components/presenter/Text/index.js
+++ src/components/presenter/Text/index.js
@@ -5,15 +5,15 @@ import { Text } from "@chakra-ui/react";
 /**
  * 
  * @param { string } comment 展示的数据
- * @param { object } rest Text样式，通过 props:{} 设置
+ * @param { object } others Text样式，通过 props:{} 设置
  * 
  */
 
 export default function Index(props) {
 
-    const { comment, ...rest } = props;
+    const { content, ...others } = props;
 
     return (
-        <Text {...rest}>{comment}</Text>
+        <Text {...others}>{content}</Text>
     )
 }
