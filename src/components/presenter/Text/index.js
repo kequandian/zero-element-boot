df891e7af169f5afd2963b94afd29adc0b246fd1
HDJ <263463764@qq.com>
2022-12-09 09:44:32 +0800

    更改Text组件注释

--- src/components/presenter/Text/index.js
+++ src/components/presenter/Text/index.js
@@ -5,8 +5,7 @@ import { Text } from "@chakra-ui/react";
 /**
  * 
  * @param { string } content 展示的数据
- * Text样式，通过 props:{} 设置
- * 文档链接 https://chakra-ui.com/docs/components/text
+ * 其他参数，参考chakra-ui/Text 的组件参数说明: https://chakra-ui.com/docs/components/text
  */
 
 export default function Index(props) {
