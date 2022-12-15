977a1e65390271384b2751b5b367ffa84c39af19
vincent@macbook <vincent@macbook.air>
2022-12-15 23:10:22 +0800

    update gateway/Filter

--- src/components/NamedGateway/index.js
+++ src/components/NamedGateway/index.js
@@ -7,18 +7,19 @@ const { default: NextIndicator } = require('../NextIndicator');
  * @param {可能是一个字符串名称} gateway
  * @param {field, filter, converter} props 
  */
-module.exports = function NamedGateway({children, xname, props, binding={}, chain={}, gateway={xname, props}, gatewaySet, ...rest }) {
+module.exports = function NamedGateway({children, xname, props, binding={}, filter={}, chain={}, gateway={xname, props}, gatewaySet, ...rest }) {
 
   const GatewaySet = gatewaySet || DefaultGatewaySet
 
   const t_binding = Object.keys(binding).length==0? undefined : binding
+  const t_filter = Object.keys(filter).length==0? undefined : filter
   const t_chain = Object.keys(chain).length==0? undefined : chain
   
-  const gatewayName = (t_binding || t_chain) ? (t_binding?'Binding':"Chain") : ( (typeof gateway === 'string')? gateway : gateway.xname )
+  const gatewayName = (t_binding||t_filter||t_chain) ? (t_binding?'Binding':(t_filter?"Filter":"Chain")) : ((typeof gateway === 'string')? gateway : gateway.xname)
   const Gateway =  gatewayName ? (GatewaySet[gatewayName]||tips(gatewayName)) : NextIndicator;
 
   // __gateway means gateway props
-  const ____gateway = (t_binding ? {binding: binding} : (t_chain? {chain: chain} : undefined)) || ((gateway && gateway.props) ? gateway.props : undefined ) || {}
+  const ____gateway = (t_binding ? {binding: binding} : (t_filter ? ({filter: filter}) : (t_chain? {chain: chain} : undefined)) ) || ((gateway && gateway.props) ? gateway.props : undefined ) || {}
   // if(gateway && gateway.props && Array.isArray(gateway.props)){
   //   gateway.props = {chain: gateway.props}
   // }
