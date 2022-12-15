16d62f5bf87662aa20901e8ee40ccf1094473e85
HDJ <263463764@qq.com>
2022-12-15 14:55:59 +0800

    调试apisUi

--- src/components/presenter/app/TopBar/index.js
+++ src/components/presenter/app/TopBar/index.js
@@ -23,7 +23,7 @@ export default function Index(props) {
         return (
             <div >
                 <div style={{ position: "absolute", left: '0%', zIndex: 20, margin: '8px' }}>
-                    <svg onClick={() => goBack()} t="1658717944661" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="12442" width="24" height="24"><path d="M624.788992 204.047974 585.205965 164.464026 219.560038 530.185011 585.205965 895.864013 624.788992 856.280986 298.663014 530.16105Z" fill='#33333390' p-id="12443"></path></svg>
+                    <svg onClick={() => goBack()} t="1658717944661" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="12442" width="24" height="24"><path d="M624.788992 204.047974 585.205965 164.464026 219.560038 530.185011 585.205965 895.864013 624.788992 856.280986 298.663014 530.16105Z" fill='#33333390' p-id="12443"></path></svg>
                 </div>
 
                 <div style={{ width: '100%', height: '100%', position: "absolute", left: '0%', zIndex: 10 , background:`${fill}`}}>
