f11d556cee52859d1fbfc5650a46158d3b7f4f71
hdj <263463764@qq.com>
2024-03-05 11:55:33 +0800

    优化组件

--- src/components/presenter/Subtitle.js
+++ src/components/presenter/Subtitle.js
@@ -4,5 +4,5 @@ export default function (props) {
 
     const { content } = props;
 
-    return <div style={{marginLeft:'6px', color: "#787878"}}>{content}</div>
+    return <div style={{ color: "#787878"}}>{content}</div>
 }
