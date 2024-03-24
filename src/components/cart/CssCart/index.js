8bf3a97510544839dc4fd6ca3862daa07ba2c65f
可圈点 <kequandian@kunmac04.local>
2024-03-24 22:25:11 +0800

    upgrade CssCart to support mulit CssCart overlay

--- src/components/cart/CssCart/index.js
+++ src/components/cart/CssCart/index.js
@@ -2,18 +2,26 @@ import React from 'react';
 
 
 /**
- * 
- * @param {style} style css样式
- * 
+ * CssCart to receive the native css style
+ * @param {object} style css样式
+ * @param {array} cssqueue 多个 CssCart 风格叠加 
  * @returns 
  */
 export default function CssCart(props) {
 
-  const { children, style }=props;
+  const { children, style, cssqueue = [], ...rest }=props;
+
+  let queuedStyles = {}
+
+  cssqueue.forEach(function(css) {
+      const {children, style, cssqueue, ...rest}  = css.props;
+      queuedStyles = {...queuedStyles, ...style, ...rest}  
+  });
 
   return React.Children.map(children, child => {
-    return <div  style={{ flex:1,  ...style}} >
-      {child}
+    return <div style={{ flex:1,  ...style, ...rest, ...queuedStyles}} >
+        {child}
     </div>
   })
 }
+
