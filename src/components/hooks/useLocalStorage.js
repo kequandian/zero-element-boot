c2b3b9ffca096f7a3eabe787f79feddeef886e76
hdj <263463764@qq.com>
2024-03-29 15:24:53 +0800

    useLocalStorage 添加注释

--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -1,5 +1,19 @@
 import { useState } from 'react';
 
+/**
+ * 
+ * 使用方法
+ * 
+ * 在组件中像使用 useState 一样来读写 localStorage 中的值
+ * 例如: 
+ * import useLocalStorage from '{path}/useLocalStorage';
+ * 
+ * ...
+ * 初始值为 'Guest'
+ * const [name, setName] = useLocalStorage('name', 'Guest');
+ * ...
+ */
+
 function useLocalStorage(key, initialValue) {
   // 从 localStorage 中读取值，如果不存在则使用初始值
   const storedValue = localStorage.getItem(key) ? JSON.parse(localStorage.getItem(key)) : initialValue;
