19f7bc475e1957aa259609c2e7b3fa4c2b1dd286
kequandian <kequandian>
2024-04-09 09:47:40 +0800

    测试 MultiActionsIndicator

---
+++ src/components/indicator/MultiActionsIndicator/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PlacementIndicator from '../PlacementIndicator'
+
+export default function MultiActionsIndicator (props) {
+
+    const { Actions=[], actions=[], alignment=[], offset } = props;
+
+    return (
+        <>
+            <PlacementIndicator>
+
+            </PlacementIndicator>
+        </>
+    )
+}
