8aa4198de2730479301f118c6156fd31e4cb0825
huangwei <646750707@qq.com>
2024-12-16 14:53:12 +0800

    feat: 调试

--- auto/config/config.default.js
+++ auto/config/config.default.js
@@ -21,16 +21,18 @@ module.exports = appInfo => {
     // myAppName: 'egg',
   };
 
-  config.listen = {
-    port: 7001,
-    hostname: '0.0.0.0', // 不建议设置为 '0.0.0.0'，可能导致外部连接风险，请了解后使用
-    // path: '/var/run/egg.sock',
-  },
+  config.cluster = {
+    listen: {
+      // path: ‘’,
+      port: 3001,
+      hostname: '0.0.0.0', //localhost
+    }
+  };
 
-    // app/config/config.default.js
-    config.jwt = {
-      secret: '12345678',
-    };
+  // app/config/config.default.js
+  config.jwt = {
+    secret: '12345678',
+  };
   config.security = {
     csrf: {
       enable: false,
