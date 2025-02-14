2348a4384d6e7faf13bbf53ec4bb872d275c8493
vincent@local.com <vincent@local.com>
2025-02-14 13:43:46 +0800

    update test.http

--- src/components/BootPreviewAutoLayout/index.js
+++ src/components/BootPreviewAutoLayout/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PreviewAutoLayout from '../PreviewAutoLayout';
 
 export default function BootPreviewAutoLayout (props) {
-  const layoutApi = '/api/aut/boot/preview'
+  const layoutApi = '/api/auto/boot/preview'
   return (
     <PreviewAutoLayout layoutApi={layoutApi} />
   )
