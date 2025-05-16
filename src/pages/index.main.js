3c112f3ef12dd3ccd7466662241f6a86c1ef7c8a
kequandian <kequandian>
2025-05-16 22:57:18 +0800

    SelectListDemo

--- src/pages/index.main.js
+++ src/pages/index.main.js
@@ -44,7 +44,7 @@ import ApisUi from '@/composition/ApisUi/Sandbox';
 //logs-ui
 import LogsUi from '@/composition/LogsUi/Sandbox'
 // SelectListDemo
-import SelectListDemo from './SelectListDemo/Sandbox'
+import SelectListDemo from './TestLists/SelectListDemo/Sandbox'
 
 import CssCart from '@/components/cart/CssCart'
 
