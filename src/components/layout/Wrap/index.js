8a4725eebf3ba3aebcb3b65e1e5c942443ec1c5e
vincent@local.com <vincent@local.com>
2025-05-21 16:06:07 +0800

    fix NamedIndicator

--- src/components/layout/Wrap/index.js
+++ src/components/layout/Wrap/index.js
@@ -1,20 +1,20 @@
 import React, {useImperativeHandle, forwardRef} from 'react';
 
 import Flexbox from '@/components/layout/Flexbox';
-// import Container from '@/components/container/Container';
-// import NextIndicator from '@/components/NextIndicator';
+import Container from '@/components/container/Container';
+import NextIndicator from '@/components/NextIndicator';
 
 export default forwardRef(function Wrap(props, ref) {
   // __ means used within AutoLayout Container
   const { children, __, ...data } = props;
 
-  // const _Container = __ ? NextIndicator : Container
+  const _Container = __ ? NextIndicator : Container
 
   return (
-    // <_Container>
+    <_Container>
         <Flexbox align='start' direction='row' {...data} ref={ref}>
           {children}
         </Flexbox>
-    // </_Container>
+    </_Container>
   )
 })
