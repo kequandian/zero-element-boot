0a771d6c8dfbbc6269114be5819d11c440f3b4f4
unknown <lenovo@newline>
2025-10-18 16:55:55 +0800

    npm publish: zero-element-boot:1.0.14

--- libCtrl.js
+++ libCtrl.js
@@ -9,14 +9,6 @@ fs.copy(`${srcPath}/assets`, `${distPath}/assets`)
   .then(() => console.log("assets folder copy"))
   .catch((e) => console.log(e));
 
-fs.copy(`${srcPath}/components/gateway/doChain.mjs`, `${distPath}/components/gateway/doChain.mjs`)
-  .then(() => console.log("doChain copy"))
-  .catch((e) => console.log(e));
-
-fs.copy(`${srcPath}/components/gateway/doFilter.mjs`, `${distPath}/components/gateway/doFilter.mjs`)
-  .then(() => console.log("doFilter copy"))
-  .catch((e) => console.log(e));
-
 fs.remove(`${distPath}/pages`)
   .then(() => console.log("pages folder deleted"))
   .catch((e) => console.log(e));
@@ -28,7 +20,6 @@ fs.remove(`${distPath}/plugins`)
 fs.remove(`${distPath}/presenter`)
   .then(() => console.log("presenter folder deleted"))
   .catch((e) => console.log(e));
-
 
 fs.remove(`${distPath}/models`)
   .then(() => console.log("models folder deleted"))
